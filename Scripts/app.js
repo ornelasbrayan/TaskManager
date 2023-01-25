8128eaffbcc523594b1eebdc19e92263721fe707
ornelasbrayan <ornelas.brayan@uabc.edu.mx>
2023-01-24 19:44:11 -0800

    class4-106

--- Scripts/app.js
+++ Scripts/app.js
@@ -19,22 +19,40 @@ function saveTask(){
     let status = $("#selStatus").val();
 
     let task = new Task(title, desc, dueDate, category, contact, status, isImportant);
-    displayTask(task);
+    // save logic
+    $.ajax({
+        type: "POST",
+        url: "https://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(task),
+        contentType: "application/json",
+        success: function(res){
+            console.log(res); // res is a JSON string
+
+            // display logic
+            displayTask(task);
+        },
+        error: function(error){
+            console.log(error);
+
+            alert("Unexpected Error");
+        }
+    });
+
 }
 
 function displayTask(task){
     let syntax = `<div class="task">
-        <div>
+        <div  class="col1">
             <h3>${task.title}</h3>
             <p>${task.description}</p>
         </div>
 
-        <div>
+        <div class="col2">
             <label>${task.dueDate}</label>
             <label>${task.category}</label>
         </div>
 
-        <div>
+        <div class="col3">
             <label>${task.contact}</label> 
             <label>${task.status}</label>
         </div>
@@ -57,9 +75,45 @@ function toggleImportant(){
     }
 }
 
+function testRequest(){
+    $.ajax({
+        type: "GET",
+        url:"https://fsdiapi.azurewebsites.net/",
+        success: function(response){
+            console.log(response);
+        },
+        error: function (error){
+            console.log(error);
+        }
+    });
+
+}
+
+function loadTasks(){
+    $.ajax({
+        type: "GET",
+        url: "https://fsdiapi.azurewebsites.net/api/tasks",
+        success: function(res){
+            let data = JSON.parse(res); // will parse the JSON string into JS objects
+            console.log(data);
+            for(let i=0; i<data.length; i++){
+                let task = data[i]; // get every obk
+                if(task.name == "Brayan"){
+                    displayTask(task);
+                }
+                
+            }
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
+}
+
 function init(){
     console.log("Task Manager");
     // loads data 
+    loadTasks();
 
     // assigns events
     $("#iImportant").click(toggleImportant);
